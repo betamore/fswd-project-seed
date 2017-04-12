aadb18de8a1a478135c494f63e00f3393ece1cdd
David Raynes <rayners@gmail.com>
2017-04-12 19:49:52 -0400

    Add protractor to test

---
+++ protractor.config.js
@@ -0,0 +1,20 @@
+exports.config = {
+  // The address of a running selenium server.
+  //seleniumAddress: 'http://localhost:4444/wd/hub',
+  directConnect: true,
+
+  // Capabilities to be passed to the webdriver instance.
+  capabilities: {
+    'browserName': 'chrome'
+  },
+
+  // Spec patterns are relative to the configuration file location passed
+  // to protractor (in this example conf.js).
+  // They may include glob patterns.
+  specs: ['example-spec.js'],
+
+  // Options to be passed to Jasmine-node.
+  jasmineNodeOpts: {
+    showColors: true, // Use colors in the command line report.
+  }
+};
